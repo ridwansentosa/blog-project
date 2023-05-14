88e6c4ae803b643c5a783b35f8dafda4a577e29d
ridwansentosa <mochridwansentosa@gmail.com>
2023-05-14 23:46:02 +0700

    create ORM

---
+++ src/main/java/com/ridwansentosa/blog/entity/Category.java
@@ -0,0 +1,65 @@
+package com.ridwansentosa.blog.entity;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description, List<Post> posts) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.posts = posts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
